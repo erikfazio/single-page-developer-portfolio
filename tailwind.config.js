2e13ec6162061d314cbe9328a70a5cae3b0650a3
erikfazio <fazioerik@gmail.com>
2023-08-20 21:39:33 +0200

    Setup html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "hsla(0, 0%, 8%, 1)",
+      green: "hsla(154, 71%, 59%, 1)",
+      darkGray: "hsla(0, 0%, 14%, 1)",
+      grey: "hsla(0, 0%, 85%, 1)",
+      white: "hsla(0, 0%, 100%, 1)",
+    },
     extend: {},
   },
   plugins: [],
