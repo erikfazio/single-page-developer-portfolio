74109acc2ba9070c9b6e51799a4eed5696b5580d
erikfazio <fazioerik@gmail.com>
2023-08-21 10:33:44 +0200

    General refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "sm": "376px",
+      "md": "768px",
+      "lg": "1440px"
+    },
     colors: {
       black: "hsla(0, 0%, 8%, 1)",
       green: "hsla(154, 71%, 59%, 1)",
@@ -16,12 +21,13 @@ export default {
       "sm": "0.75rem",
       "base": "1rem",
       "lg": "1.125rem",
-      "xl": "2.5rem",
-      "2xl": "2rem",
-      "3xl": "2.5rem",
-      "4xl": "3rem",
-      "5xl": "4.5rem",
-      "6xl": "5.5rem",
+      "xl": "1.5rem",
+      "2xl": "2.5rem",
+      "3xl": "2rem",
+      "4xl": "2.5rem",
+      "5xl": "3rem",
+      "6xl": "4.5rem",
+      "7xl": "5.5rem",
     },
     extend: {},
   },
